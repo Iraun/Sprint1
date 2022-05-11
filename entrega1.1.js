1c05516de3ddccbb232430d5b04590cff1851287
Ptwin <pereserra97@gmail.com>
2022-05-11 11:57:18 +0200

    comit primera entrega

---
+++ entrega1.1.js
@@ -0,0 +1,50 @@
+//NIVELL 1 EXERCICI 1
+
+function diguesnom(nom){
+    console.log(nom) 
+    
+ } 
+ diguesnom("Pere")
+
+ //NIVELL 2 EXERCICI 1
+
+    let nom2 = "Pere";
+    let cognom = "Serra";
+
+    console.log(`${nom2} ${cognom}`)
+
+//NIVELL 2 EXERCICI 2
+
+function valor() {
+    let hello = 'Hello World';
+    return `${hello}`;
+}
+
+console.log(valor());
+
+//NIVELL 3 EXERCICI 1
+
+let arrFuncions = [];
+
+function counter(){
+    let num = 0
+    for(j=1; j<10; j++){
+        num += [j]
+    }
+    //console.log(num)
+    return num;
+}
+
+for(i=0; i<10; i++ ){
+    arrFuncions.push(counter()) 
+}  
+    
+console.log(arrFuncions);
+
+
+//NIVELL 3 EXERCICI 2
+
+(function( param1 ){
+    console.log( param1 ); //Pere
+})( 'Pere' );
+
